181f2ce1c119856212b86545c3150da7d5769dd2
Raluca Scortanu <raluca.scortanu@gmail.com>
2017-06-10 00:49:40 +0300

    json and xml controller

---
+++ TeaSk.Web/Controllers/JsonController.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TeaSk.Application.Infrastructure;
+using TeaSk.Domain.Entities;
+
+namespace TeaSk.Web.Controllers
+{
+    public class JsonController : ApiController
+    {
+        private readonly IService<Activities> _activitiesService;
+        private readonly IService<User> _userService;
+        public JsonController(IService<Activities> activitiesService, IService<User> userService)
+        {
+            _activitiesService = activitiesService;
+            _userService = userService;
+        }
+
+        public string Users()
+        {
+            return JsonConvert.SerializeObject(_userService.GetAll());
+        }
+
+        public string Events()
+        {
+            return JsonConvert.SerializeObject(_activitiesService.GetAll());
+        }
+    }
+}
